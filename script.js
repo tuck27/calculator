3164dff5f9df6f275da12a16ae52abe0619ace28
Maya <mayas2744@tuckahoeschools.org>
2025-02-25 13:30:21 +0000

    second commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const oneButton = document.getElementById("one").value = 1
+const twoButton = document.getElementById("two").value = 2
+const threeButton = document.getElementById("three").value = 3
+const fourButton = document.getElementById("four").value = 4
+const fiveButton = document.getElementById("five").value = 5
+const sixButton = document.getElementById("six").value = 6
+const sevenButton = document.getElementById("seven").value = 7
+const eightButton = document.getElementById("eight").value = 8
+const nineButton = document.getElementById("nine").value = 9
+const zeroButton = document.getElementById("zero").value = 0
+const addButton = document.getElementById("add")
+const subButton = document.getElementById("subtract")
+const multButton = document.getElementById("multiply")
+const divButton = document.getElementById("divide")
+const percentButton = document.getElementById("percentage")
+const decButton = document.getElementById("decimal-point")
+const exponentButton = document.getElementById("exponent")
+const clearButton = document.getElementById("clear")
+const ansButton = document.getElementById("equal-sign")
+const negativeButton = document.getElementById("negative")
+const piButton = document.getElementById("pi")
