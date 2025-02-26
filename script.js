2245572d3483c715203305faab24812bd06ba3c1
Maya <mayas2744@tuckahoeschools.org>
2025-02-26 13:32:07 +0000

    something commit

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
-const oneButton = document.getElementById("one").value = 1
-const twoButton = document.getElementById("two").value = 2
-const threeButton = document.getElementById("three").value = 3
-const fourButton = document.getElementById("four").value = 4
-const fiveButton = document.getElementById("five").value = 5
-const sixButton = document.getElementById("six").value = 6
-const sevenButton = document.getElementById("seven").value = 7
-const eightButton = document.getElementById("eight").value = 8
-const nineButton = document.getElementById("nine").value = 9
-const zeroButton = document.getElementById("zero").value = 0
-const addButton = document.getElementById("add")
-const subButton = document.getElementById("subtract")
-const multButton = document.getElementById("multiply")
-const divButton = document.getElementById("divide")
-const percentButton = document.getElementById("percentage")
-const decButton = document.getElementById("decimal-point")
-const exponentButton = document.getElementById("exponent")
-const clearButton = document.getElementById("clear")
-const ansButton = document.getElementById("equal-sign")
-const negativeButton = document.getElementById("negative")
-const piButton = document.getElementById("pi")
+//Function to display value
+function dis(val) {
+    document.getElementById("result").value += val
+}
+function result(event) {
+    if (event.key == '0'|| event.key == '1'
+        || event.key == '2' || event.key =='3'
+        || event.key == '4' || event.key =='5'
+        || event.key == '6' || event.key =='7'
+        || event.key == '8' || event.key =='9'
+        || event.key == '+' || event.key =='-'
+        || event.key == '*' || event.key =='/')
+    document.getElementById("result").value += event.key;
+}
+const cal = document.getElementById("calculator")
+cal.onkeyup = function (event) {
+    if(event.keyCode === 13) {
+        console.log("Enter");
+        let x = document.getElementById("result").value
+        console.log(x);
+        solve();
+    }
+}
+//Function that returns the result
+function solve() {
+    let x = document.getElementById("result").value
+    let y = Math.evaluate(x)
+    document.getElementById("result").value = y
+}
+
+//Function to clear calculator
+function clear() {
+    document.getElementById("result").value = ""
+}
