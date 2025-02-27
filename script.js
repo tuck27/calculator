3f03e38441d3ccb79c58be9a200fa8c9310369ae
Maya <mayas2744@tuckahoeschools.org>
2025-02-27 13:32:21 +0000

    idk commit

--- script.js
+++ script.js
@@ -1,34 +1,42 @@
-//Function to display value
-function dis(val) {
-    document.getElementById("result").value += val
-}
-function result(event) {
-    if (event.key == '0'|| event.key == '1'
-        || event.key == '2' || event.key =='3'
-        || event.key == '4' || event.key =='5'
-        || event.key == '6' || event.key =='7'
-        || event.key == '8' || event.key =='9'
-        || event.key == '+' || event.key =='-'
-        || event.key == '*' || event.key =='/')
-    document.getElementById("result").value += event.key;
-}
-const cal = document.getElementById("calculator")
-cal.onkeyup = function (event) {
-    if(event.keyCode === 13) {
-        console.log("Enter");
-        let x = document.getElementById("result").value
-        console.log(x);
-        solve();
-    }
-}
-//Function that returns the result
-function solve() {
-    let x = document.getElementById("result").value
-    let y = Math.evaluate(x)
-    document.getElementById("result").value = y
-}
+const calculator = document.querySelector(".calculator");
+const keys = document.querySelector(".calculator_keys");
 
-//Function to clear calculator
-function clear() {
-    document.getElementById("result").value = ""
-}
+keys.addEventListener("click", e => {
+    if (e.target.matches('button')) {
+        //do something
+    }
+})
+
+
+const display = document.querySelector('.calculator_display')
+
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+        const key = e.target
+        const action = key.dataset.action
+        const keyContent = key.textContent
+        const displayedNum = display.textContent
+    }
+    if (!action) {
+        if (displayedNum === '0') {
+            display.textContent = keyContent
+        } else {
+            display.textContent = displayedNum + keyContent
+        }
+    }
+    if (action === 'decimal') {
+        display.textContent = displayedNum + '.'
+    }
+    if (
+        action === 'add' ||
+        action === 'subtract' ||
+        action === 'multiply' ||
+        action === 'divide'
+    ) {
+        key.classList.add('is-depressed')
+   
+    // Remove .is-depressed class from all keys
+    Array.from(key.parentNode.children)
+        .forEach(k => k.classList.remove('is-depressed'))
+    }
+})
